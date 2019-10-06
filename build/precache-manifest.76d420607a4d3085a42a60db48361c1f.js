6a6aac7f12d83d891589e5c4e0441c64568ccbab
megaman18 <smhk2000@gmail.com>
2019-10-06 18:59:02 +0500

    5th commit

---
+++ build/precache-manifest.76d420607a4d3085a42a60db48361c1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123860e7ecd939a2b9760ca1047acaa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b3a27982e4b2f4087f5",
+    "url": "/static/css/main.707c1bf0.chunk.css"
+  },
+  {
+    "revision": "fffbb4eb4d3816466cf5",
+    "url": "/static/js/2.eaf6e765.chunk.js"
+  },
+  {
+    "revision": "6b3a27982e4b2f4087f5",
+    "url": "/static/js/main.079722bd.chunk.js"
+  },
+  {
+    "revision": "2b23afccef661d6bb6fa",
+    "url": "/static/js/runtime-main.0c33401d.js"
+  }
+]);
