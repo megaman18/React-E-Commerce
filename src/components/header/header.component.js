425756593fb3c4d91d54e13d842d3ebf74880cd8
megaman18 <smhk2000@gmail.com>
2019-10-08 16:25:42 +0500

    signin changes

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -15,6 +15,9 @@ const Header = () => (
      <Link className='option' to='/shop'>
          CONTACT
      </Link>
+     <Link className='option' to='/signin'>
+         signin
+     </Link>
     </div>              
   </div>
 );
